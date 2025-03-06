71bd3e6052c259918ca40c327f13a994a3011ca9
majdarina <majorskimajda98@icloud.com>
2025-03-06 17:30:10 +0100

    mm

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = { // Ensures static export for GitHub Pages
+const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // Fixes images not working on GitHub Pages
-    domains: ["upload.wikimedia.org"], // Allows external images
+    unoptimized: true,
+    domains: ["upload.wikimedia.org"],
   },
-  basePath: "/black-box-media", // Update if your repo name is different
+  basePath: "/black-box-media", 
   assetPrefix: "/black-box-media/",
+  webpack(config) {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
-
